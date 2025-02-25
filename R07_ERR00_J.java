b619113905fab22aba9bdd6bd979ebaad633a607
Alex Chavez <al230470@ucf.edu>
2025-02-25 22:47:34 +0000

    Uploaded Correct Code for R07_ERR00_J.java

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -1,5 +1,16 @@
-try {
-  //...
-} catch (IOException ioe) {
-  ioe.printStackTrace();
-}
+// Rule 07: Exceptional Behavior (ERR)
+// Do not suppress or ignore checked exceptions
+
+
+boolean validFlag = false;
+do {
+  try {
+    // ...
+    // If requested file does not exist, throws FileNotFoundException
+    // If requested file exists, sets validFlag to true
+    validFlag = true;
+  } catch (FileNotFoundException e) {
+    // Ask the user for a different file name
+  }
+} while (validFlag != true);
+// Use the file
