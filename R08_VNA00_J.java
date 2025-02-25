dba567b3f432471a2be8cde1c59b54059fbf22ab
Alex Chavez <al230470@ucf.edu>
2025-02-25 22:48:38 +0000

    Add files via upload

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -1,18 +1,22 @@
+// Rule 08: Visibility and Atomicity (VNA)
+// Ensure visibility when accessing shared primitive variables 
+
+
 final class ControlledStop implements Runnable {
-  private boolean done = false;
-  
-  @Override public void run() {
-    while (!done) {
-      try {
-        // ...
-        Thread.currentThread().sleep(1000); // Do something
-      } catch(InterruptedException ie) {
-        Thread.currentThread().interrupt(); // Reset interrupted status
-      }
-    }   
+    private volatile boolean done = false;
+    
+    @Override public void run() {
+      while (!done) {
+        try {
+          // ...
+          Thread.currentThread().sleep(1000); // Do something
+        } catch(InterruptedException ie) {
+          Thread.currentThread().interrupt(); // Reset interrupted status
+        }
+      }   
+    }
+   
+    public void shutdown() {
+      done = true;
+    }
   }
- 
-  public void shutdown() {
-    done = true;
-  }
-}
