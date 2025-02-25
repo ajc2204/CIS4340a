f63833abb1a698797a2e4059c8078d136d5c0dae
Alex Chavez <al230470@ucf.edu>
2025-02-25 22:33:54 +0000

    Uploading Correct Code for R01_DCL00_J.java

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -1,12 +1,14 @@
+// Rule 01: Declarations and Initialization (DCL) 
+// Prevent Class Initialization Cycles 
+
 public class Cycle {
   private final int balance;
-  private static final Cycle c = new Cycle();
   private static final int deposit = (int) (Math.random() * 100); // Random deposit
- 
+  private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
   public Cycle() {
     balance = deposit - 10; // Subtract processing fee
   }
- 
+  
   public static void main(String[] args) {
     System.out.println("The account balance is: " + c.balance);
   }
